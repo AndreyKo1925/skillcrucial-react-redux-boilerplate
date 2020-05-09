6a7bc207a5b6b58c4188cbe03d655011de3096fb
Andrey <kochmuradov@gmail.com>
2020-05-09 18:00:58 +0300

    project task 11.1

--- server/server.js
+++ server/server.js
@@ -4,14 +4,41 @@ import path from 'path'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import sockjs from 'sockjs'
-
+import axios from 'axios'
 import cookieParser from 'cookie-parser'
 import Html from '../client/html'
+// import { unlink } from 'fs'
+
+const { writeFile, readFile, unlink } = require('fs').promises
 
 let connections = []
 
 const port = process.env.PORT || 3000
 const server = express()
+
+// const setHeaders = (req, res, next) => {
+//   res.set('x-skillcrucial-user', '743160e0-31a2-4bb8-928a-3da7ee2cb896')
+//   res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+//   next()
+// }
+
+const deleteFile = async (namefile) => {
+  return unlink(`${__dirname}/${namefile}`)
+}
+
+const saveFile1 = async (users, namefile) => {
+  return writeFile(`${__dirname}/${namefile}`, JSON.stringify(users), { encoding: 'utf8' })
+}
+
+const readFile1 = async (namefile) => {
+  return readFile(`${__dirname}/${namefile}`, { encoding: 'utf8' })
+    .then((data) => JSON.parse(data))
+    .catch(async () => {
+      const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
+      await saveFile1(users, namefile)
+      return users
+    })
+}
 
 server.use(cors())
 
@@ -20,6 +47,53 @@ server.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit
 server.use(bodyParser.json({ limit: '50mb', extended: true }))
 
 server.use(cookieParser())
+server.get('/api/v1/users/', async (req, res) => {
+  const users = await readFile1('test.json')
+  res.set('x-skillcrucial-user', '743160e0-31a2-4bb8-928a-3da7ee2cb896')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  res.json(users)
+})
+
+server.post('/api/v1/users/', async (req, res) => {
+  let users = await readFile1('test.json')
+  const newUser = req.body
+  newUser.id = users[users.length - 1].id + 1
+  users = [...users, newUser]
+  await saveFile1(users, 'test.json')
+  res.set('x-skillcrucial-user', '743160e0-31a2-4bb8-928a-3da7ee2cb896')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  res.json({ status: 'success', id: newUser.id })
+})
+
+server.patch('/api/v1/users/:number', async (req, res) => {
+  const users = await readFile1('test.json')
+  const { number } = req.params
+  const modUser = req.body
+  const indexModUser = users.indexOf(users.find((it) => +it.id === +number))
+  users[indexModUser] = { ...users[indexModUser], ...modUser }
+  await saveFile1(users, 'test.json')
+  res.set('x-skillcrucial-user', '743160e0-31a2-4bb8-928a-3da7ee2cb896')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  res.json({ status: 'success', id: number })
+})
+
+server.delete('/api/v1/users/:number', async (req, res) => {
+  const users = await readFile1('test.json')
+  const { number } = req.params
+  const indexModUser = users.indexOf(users.find((it) => +it.id === +number))
+  users.splice(indexModUser, 1)
+  await saveFile1(users, 'test.json')
+  res.set('x-skillcrucial-user', '743160e0-31a2-4bb8-928a-3da7ee2cb896')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  res.json({ status: 'success', id: number })
+})
+
+server.delete('/api/v1/users/', async (req, res) => {
+  await deleteFile('test.json')
+  res.set('x-skillcrucial-user', '743160e0-31a2-4bb8-928a-3da7ee2cb896')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  res.json({ status: 'ok' })
+})
 
 server.use('/api/', (req, res) => {
   res.status(404)
@@ -29,7 +103,7 @@ server.use('/api/', (req, res) => {
 const echo = sockjs.createServer()
 echo.on('connection', (conn) => {
   connections.push(conn)
-  conn.on('data', async () => {})
+  conn.on('data', async () => { })
 
   conn.on('close', () => {
     connections = connections.filter((c) => c.readyState !== 3)
